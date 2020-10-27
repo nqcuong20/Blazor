f26c1dcce61a607740b7975765644efd0b12841e
Nguyễn Quốc Cường <60700939+nqcuong20@users.noreply.github.com>
2020-10-27 10:30:45 +0700

    up web todo

---
+++ ToDo_SpaceTeam/Data/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo_SpaceTeam.Data
+{ 
+        public class TodoItem
+        {
+            public string Title { get; set; }
+            public bool Done { get; set; }
+            public string Note { get; set; }
+        }
+}
+
---
+++ ToDo_SpaceTeam/Data/TodoItemService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using Newtonsoft.Json;
+
+namespace ToDo_SpaceTeam.Data
+{
+    public class TodoItemService
+    {
+        private static List<TodoItem> _data = new List<TodoItem>() {
+            new TodoItem{ Title = "Meet Donald Trump"},
+            new TodoItem{ Title = "Lunch with Barack Obama"},
+            new TodoItem{ Title = "Go fishing with Bill Gates"},
+            new TodoItem{ Title = "Walking with Putin"},
+        };
+
+        private readonly string _file = "Data\\todo.json";
+
+        public List<TodoItem> GetData()
+        {
+            if (File.Exists(_file))
+            {
+                using var file = File.OpenText(_file);
+                var serializer = new JsonSerializer();
+                _data = serializer.Deserialize(file, typeof(List<TodoItem>)) as List<TodoItem>;
+            }
+            return _data;
+        }
+
+        public void SaveChanges()
+        {
+            using var file = File.CreateText(_file);
+            var serializer = new JsonSerializer();
+            serializer.Serialize(file, _data);
+        }
+    }
+}
